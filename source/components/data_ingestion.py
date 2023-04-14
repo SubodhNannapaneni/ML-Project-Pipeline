b0df8d10832a2d6c6efd01a57c4cec2560c38212
Subodh Nannapaneni <subodhnannapaneni@gmail.com>
2023-04-14 15:11:57 +0530

    basic model trainer and transformation

--- source/components/data_ingestion.py
+++ source/components/data_ingestion.py
@@ -6,11 +6,16 @@ import pandas as  pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from source.components.data_transformation import DataTransformation
+from source.components.data_transformation import DataTransformationConfig
+
+from source.components.model_trainer import ModelTrainer, ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
-    train_data_path:str = os.path.join('artifacts', 'train.csv')
-    test_data_path:str = os.path.join('artifacts', 'test.csv')
-    raw_data_path:str = os.path.join('artifacts', 'raw_data.csv')
+    train_data_path:str = os.path.join('data', 'train.csv')
+    test_data_path:str = os.path.join('data', 'test.csv')
+    raw_data_path:str = os.path.join('data', 'raw_data.csv')
     
 class DataIngestion:
     def __init__(self):
@@ -21,6 +26,10 @@ class DataIngestion:
         try:
             df = pd.read_csv('data\StudentsPerformance.csv')
             logging.info('read the data as df')
+            
+            # move math score column to the end
+            math_score_col = df.pop('math score')
+            df['math score'] = math_score_col
             
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             
@@ -44,4 +53,10 @@ class DataIngestion:
         
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformations(train_data, test_data)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
