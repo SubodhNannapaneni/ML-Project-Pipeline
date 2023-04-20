4fd679eea43c7213748f2eeef30f64652d9993fe
Subodh Nannapaneni <subodhnannapaneni@gmail.com>
2023-04-20 14:38:49 +0530

    added predict and web app

--- source/utils.py
+++ source/utils.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 import numpy as np
 import dill
+import pickle
 
 from source.exceptions import CustomException
 from sklearn.metrics import r2_score
@@ -40,6 +41,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
